cc3fbf1b5ee9f1f83a5684f90bdc707e026965df
Stitchey <erikerik5@hotmail.com>
2014-09-25 18:53:34 +0200

    Gissa Talet

---
+++ LBS_lektion_GissaTalet/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBS_lektion_GissaTalet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numberU = 0;
+            string restart;
+    
+            Random random = new Random();
+            int numberC = random.Next(1, 11);
+
+            while (numberC != numberU)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter a number from one to ten");
+                numberU = Convert.ToInt32(Console.ReadLine());
+
+                Console.Clear();
+
+                if (numberC > numberU)
+                    Console.WriteLine("För lågt! försök igen.");
+
+                else if (numberC < numberU)
+                    Console.WriteLine("För högt! försök igen.");
+
+                else
+                {
+                    Console.WriteLine("Grattis!");
+                    Console.WriteLine("Vill du starta igen? ");
+                    Console.WriteLine("Y/N");
+
+                    restart = Console.ReadLine().ToLower();
+
+                    if (restart == "y")
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LBS_lektion_GissaTalet/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LBS_lektion_GissaTalet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LBS_lektion_GissaTalet")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59accc7b-634d-4558-9c7f-9b5658c4277b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
